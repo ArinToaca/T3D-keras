d65f6f961a6d3a0be724dcf977c5e2c593fec3be
Arin Toaca <arin.toaca@gmail.com>
2018-12-08 19:49:44 +0200

    Fix various readability issues

--- make_train_test_split.py
+++ make_train_test_split.py
@@ -2,19 +2,19 @@ from random import shuffle
 import csv
 import glob
 
-action_classes = ['HorseRace', 'YoYo', 'SoccerJuggling', 'MilitaryParade', 'BaseballPitch', 'JugglingBalls', 'BenchPress', 'Biking', 'VolleyballSpiking', 'PlayingGuitar', 'ThrowDiscus', 'SalsaSpin', 'PlayingPiano', 'PoleVault', 'Mixing', 'PlayingViolin', 'CleanAndJerk', 'Basketball', 'HulaHoop', 'JumpingJack', 'RopeClimbing', 'GolfSwing', 'PizzaTossing', 'Fencing', 'TrampolineJumping', 'Billiards', 'Nunchucks', 'PommelHorse', 'SkateBoarding', 'HorseRiding', 'TennisSwing', 'TaiChi', 'Diving', 'Drumming', 'WalkingWithDog', 'Skijet', 'Lunges', 'Rowing', 'PlayingTabla', 'Punch', 'BreastStroke', 'RockClimbingIndoor', 'JavelinThrow', 'PushUps', 'Kayaking', 'Skiing', 'Swing', 'PullUps', 'JumpRope', 'HighJump']
+action_classes = ['Fight']
 print(len(set(action_classes)))
+
+
 def create_csvs():
     train = []
     test = []
-    total_train = []
-    total_test = []
 
     for myclass, directory in enumerate(action_classes):
         for filename in glob.glob('UCF50/{}/*.avi'.format(directory)):
             group = ((filename.split('/')[-1]).split('.')[0]).split('_')[-2]
 
-            if group in ['g01','g02','g02','g04', 'g05']:
+            if group in ['g01', 'g02', 'g02', 'g04', 'g05']:
                 test.append([filename, myclass, directory])
             else:
                 train.append([filename, myclass, directory])
--- train_T3D_keras.py
+++ train_T3D_keras.py
@@ -2,16 +2,17 @@
 import os
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
-from keras.optimizers import Adam, SGD
+from keras.optimizers import Adam
 import keras.backend as K
 import traceback
 
-from T3D_keras import densenet161_3D_DropOut, densenet121_3D_DropOut
+from T3D_keras import densenet161_3D_DropOut
 from get_video import video_gen
 
-# there is a minimum number of frames that the network must have, values below 10 gives -- ValueError: Negative dimension size caused by subtracting 3 from 2 for 'conv3d_7/convolution'
+# there is a minimum number of frames that the network must have, values below
+# 10 gives -- ValueError: Negative dimension size caused by subtracting 3 from
+# 2 for 'conv3d_7/convolution'
 # paper uses 224x224, but in that case also the above error occurs
 FRAMES_PER_VIDEO = 20
 FRAME_HEIGHT = 256
@@ -25,7 +26,8 @@ MODEL_FILE_NAME = 'T3D_saved_model.h5'
 
 def train():
     sample_input = np.empty(
-        [FRAMES_PER_VIDEO, FRAME_HEIGHT, FRAME_WIDTH, FRAME_CHANNEL], dtype=np.uint8)
+        [FRAMES_PER_VIDEO, FRAME_HEIGHT, FRAME_WIDTH, FRAME_CHANNEL],
+        dtype=np.uint8)
 
     # Read Dataset
     d_train = pd.read_csv(os.path.join('train.csv'))
@@ -34,28 +36,32 @@ def train():
     nb_classes = len(set(d_train['class']))
 
     video_train_generator = video_gen(
-        d_train, FRAMES_PER_VIDEO, FRAME_HEIGHT, FRAME_WIDTH, FRAME_CHANNEL, nb_classes, batch_size=BATCH_SIZE)
+        d_train, FRAMES_PER_VIDEO, FRAME_HEIGHT, FRAME_WIDTH, FRAME_CHANNEL,
+        nb_classes, batch_size=BATCH_SIZE)
     video_val_generator = video_gen(
-        d_valid, FRAMES_PER_VIDEO, FRAME_HEIGHT, FRAME_WIDTH, FRAME_CHANNEL, nb_classes, batch_size=BATCH_SIZE)
+        d_valid, FRAMES_PER_VIDEO, FRAME_HEIGHT, FRAME_WIDTH, FRAME_CHANNEL,
+        nb_classes, batch_size=BATCH_SIZE)
 
     # Get Model
     # model = densenet121_3D_DropOut(sample_input.shape, nb_classes)
     model = densenet161_3D_DropOut(sample_input.shape, nb_classes)
 
-    checkpoint = ModelCheckpoint('T3D_saved_model_weights.hdf5', monitor='val_loss',
-                                 verbose=1, save_best_only=True, mode='min', save_weights_only=True)
+    checkpoint = ModelCheckpoint('T3D_saved_model_weights.hdf5',
+                                 monitor='val_loss',
+                                 verbose=1, save_best_only=True, mode='min',
+                                 save_weights_only=True)
     earlyStop = EarlyStopping(monitor='val_loss', mode='min', patience=100)
     reduceLROnPlat = ReduceLROnPlateau(monitor='val_loss', factor=0.5,
                                        patience=20,
-                                       verbose=1, mode='min', min_delta=0.0001, cooldown=2, min_lr=1e-6)
+                                       verbose=1, mode='min', min_delta=0.0001,
+                                       cooldown=2, min_lr=1e-6)
 
     callbacks_list = [checkpoint, reduceLROnPlat, earlyStop]
 
     # compile model
     optim = Adam(lr=1e-4, decay=1e-6)
-    #optim = SGD(lr = 0.1, momentum=0.9, decay=1e-4, nesterov=True)
-    model.compile(optimizer=optim, loss='categorical_crossentropy', metrics=['accuracy'])
-    
+    model.compile(optimizer=optim, loss='categorical_crossentropy',
+                  metrics=['accuracy'])
     if os.path.exists('./T3D_saved_model_weights.hdf5'):
         print('Pre-existing model weights found, loading weights.......')
         model.load_weights('./T3D_saved_model_weights.hdf5')
@@ -87,5 +93,6 @@ if __name__ == '__main__':
         print('Error:', err)
         traceback.print_tb(err.__traceback__)
     finally:
-        # Destroying the current TF graph to avoid clutter from old models / layers
+        # Destroying the current TF graph to avoid clutter from old
+        # models / layers
         K.clear_session()
